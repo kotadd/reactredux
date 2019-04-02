5e159e17d19b20293ed245a4c06c0296b2bcbed1
Kota Nishinaka <private2030kota@gmail.com>
2019-04-03 08:24:39 +0900

    Implement console

--- server/build/bundle.js
+++ server/build/bundle.js
@@ -8954,10 +8954,13 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express2.default)();
 
+app.use(_express2.default.static('public'));
 app.get('/', function (req, res) {
   var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
-  res.send(content);
+  var html = '\n    <html>\n      <head></head>\n      <body>\n        <div id="root">' + content + '</div>\n        <script src="bundle.js"></script>\n      </body>\n    </html>\n  ';
+
+  res.send(html);
 });
 
 app.listen(3000, function () {
--- server/src/index.js
+++ server/src/index.js
@@ -10,11 +10,21 @@ import Home from './client/components/Home';
 
 const app = express();
 
-
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
 
-  res.send(content);
+  const html = `
+    <html>
+      <head></head>
+      <body>
+        <div id="root">${content}</div>
+        <script src="bundle.js"></script>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
 });
 
 app.listen(3000, () => {
